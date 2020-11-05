78971acb98e8ce097fcb8773ed4558520f8930ff
MARCOS DE OLIVEIRA CORREA <marcosocorrea@gmail.com>
2020-11-04 21:56:12 -0300

    04 11 2020

---
+++ src/components/Media.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from './Card'
+
+export default props => {
+
+    return (
+        <Card title="Média dos Números" blue>
+            <div>
+                <span>
+                    <span>Resultado:</span>
+                    <strong>{10}</strong>
+                </span>
+            </div> 
+        </Card>
+    )
+}
+
---
+++ src/components/Sorteio.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from './Card'
+
+export default props => {
+
+    return (
+        <Card title="Sorteio de um Número" purple>
+            <div>
+                <span>
+                    <span>Resultado:</span>
+                    <strong>{10}</strong>
+                </span>
+            </div> 
+        </Card>
+    )
+}
+
